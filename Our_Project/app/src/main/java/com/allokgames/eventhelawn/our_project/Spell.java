8d2d9720f690e01ecf4e1bc256db35182db9fbb0
eventhelawn <g_gazinur_g98@mail.ru>
2017-02-14 17:28:47 +0300

    Идеи и классы в новом проекте

---
+++ Our_Project/app/src/main/java/com/allokgames/eventhelawn/our_project/Spell.java
@@ -0,0 +1,7 @@
+package com.allokgames.eventhelawn.our_project;
+
+/**
+ * Created by User on 14.02.2017.
+ */
+public class Spell {
+}
---
+++ Our_Project/app/src/main/java/com/allokgames/eventhelawn/our_project/Weakneses.java
@@ -0,0 +1,31 @@
+package com.allokgames.eventhelawn.our_project;
+
+/**
+ * Created by User on 14.02.2017.
+ */
+public class Weakneses {
+    private String type;
+    private int value;
+
+    public Weakneses(String type, int value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getType() {
+
+        return type;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}

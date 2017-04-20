d2ca801fc564e3884af809275cd684e826914698
eventhelawn <g_gazinur_g98@mail.ru>
2017-04-20 21:35:11 +0300

    daily commit

--- Our_Project/app/src/main/java/com/allokgames/eventhelawn/our_project/Controller/BattleActivity.java
+++ Our_Project/app/src/main/java/com/allokgames/eventhelawn/our_project/Controller/BattleActivity.java
@@ -3,16 +3,19 @@ package com.allokgames.eventhelawn.our_project.Controller;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.allokgames.eventhelawn.our_project.R;
-
 /**
  * Created by User on 20.04.2017.
  */
 public class BattleActivity extends Activity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.battle);
+        mRecyclerView = (RecyclerView)findViewById(R.id.my_recycler_view);
     }
 }
